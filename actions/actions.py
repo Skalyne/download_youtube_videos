9faa7c836d8f2be051d48df624dc0fb21d5a66a0
theo kratz toribio <theo.kratz.t@gmail.com>
2022-10-01 12:16:28 +0200

    working

--- actions/actions.py
+++ actions/actions.py
@@ -2,10 +2,10 @@ from pytube import YouTube
 from tkinter import messagebox as MessageBox
 from tkinter import Label, OptionMenu, StringVar
 
-def download_accion(video_url):
+def download_accion(video_url, output_path = None):
     video = YouTube(video_url.get())
     download = video.streams.get_highest_resolution()
-    download.download()
+    download.download(output_path=output_path)
 
 def author_info_popup():
     MessageBox.showinfo("about me:", "Skalyne")
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+from tkinter import filedialog
 from pytube import YouTube
 import os
 from tkinter import *
 from tkinter import messagebox as MessageBox
 from PIL import Image, ImageTk
 from actions.actions import author_info_popup, download_accion
-class App(Frame):
 
+class App(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.parent.geometry("480x320")
         self.config(bd=15, width=480,height=320)
         self.parent.title("Download youtube videos")
+        self.folder_path= StringVar()
+        self.filename = None
         self.set_logo()
         self.menu_bar()
         self.instructions()
         self.entry_url()
         self.download_button()
+        self.browse_button()
+
+    def browse_button(self):
+        self.lbl1 = Label(master=self.parent,textvariable=self.folder_path)
+        self.lbl1.grid(row=3, column=1)
+        self.button2 = Button(text="Browse", command=self.browse_action)
+        self.button2.grid(row=2, column=2, sticky=W)
+
+    def browse_action(self):
+        # Allow user to select a directory and store it in global var
+        # called folder_path
+        self.filename = filedialog.askdirectory()
+        self.folder_path.set(self.filename)
+        print(self.filename)
 
     def set_logo(self):
         self.img = ImageTk.PhotoImage(Image.open('img/youtube-logo.png').resize(size=[60, 60]))
@@ -40,8 +57,8 @@ class App(Frame):
         self.video_url.grid(row=1,column=1, sticky=EW, columnspan=2)
 
     def download_button(self):
-        self.download_btn = Button(root, text="download", command= lambda: download_accion(self.video_url))
-        self.download_btn.grid(row=2,column=1)
+        self.download_btn = Button(root, text="download", command= lambda: download_accion(self.video_url, self.filename))
+        self.download_btn.grid(row=2,column=1, sticky=W)
 
 if __name__ == "__main__":
     root = Tk()
